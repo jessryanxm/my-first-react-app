19b78c4d50fecef1219afcac2445cef60cdea979
Jess Ryan <jess.ryan@comcast.com>
2020-06-30 14:52:35 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://media.giphy.com/media/8P7y4Ybt9fEsfEkScl/giphy.gif" className="App-logo" alt="logo" />
+        <p>
+          I'm learning how to write JavaScript!
+        </p>
+        <a
+          class="App-link" href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hooray
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
