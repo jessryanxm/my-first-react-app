4552c512044733004c223084524bc7e448bb7028
Jess Ryan <jess.ryan@comcast.com>
2020-07-02 13:33:06 -0400

    Updates

---
+++ precache-manifest.fb03cbafb6e176c4696cacfa88462c44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a24ced1c304a9e2b3f1f3153f1b85ba1",
+    "url": "/my-first-react-app/index.html"
+  },
+  {
+    "revision": "fb975426b1eb2b6b3fd7",
+    "url": "/my-first-react-app/static/css/main.dffbc398.chunk.css"
+  },
+  {
+    "revision": "4243468e1712c338e9c0",
+    "url": "/my-first-react-app/static/js/2.537834b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-first-react-app/static/js/2.537834b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb975426b1eb2b6b3fd7",
+    "url": "/my-first-react-app/static/js/main.56fc818b.chunk.js"
+  },
+  {
+    "revision": "9fa5d64e5b1bb9a77b80",
+    "url": "/my-first-react-app/static/js/runtime-main.c3e2ea89.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-first-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
